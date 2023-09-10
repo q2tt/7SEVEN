e68fe88b04068dbd8575cf4f629d581efd98acde
q22tt <alenaartamonova066@gmail.com>
2023-09-10 19:25:16 +0300

    nav mobile

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,9 @@
 const langUa = document.getElementById('lang-ua')
 const langEn= document.getElementById('lang-en')
+const menuBtn = document.getElementById('menu-btn');
+const menu = document.getElementById('menu-links');
+const menuItem = document.querySelectorAll('.nav__link');
+
 
 langUa.addEventListener('click', () => {
     if(!langUa.classList.contains('language__item_active')){
@@ -18,4 +22,19 @@ function toggleClasses(){
     langUa.classList.toggle("language__item_no-act");
     langEn.classList.toggle("language__item_active");
     langEn.classList.toggle("language__item_no-act");
-}
+}  
+
+
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+	menu.classList.toggle('active');
+})
+
+menuItem.forEach (function(menuItem) {
+  menuItem.addEventListener('click',function(){
+          menuBtn.classList.toggle('active');
+          menu.classList.toggle('active');
+  })
+})
+
+
